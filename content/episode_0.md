a7a4da04d9f53cf1fc3ae1308cc2216d840dfe98
Tobias Macey <tmacey@boundlessnotions.com>
2015-04-09 05:53:56 -0400

    Initial setup of podcastinit static site

---
+++ content/episode_0.md
@@ -0,0 +1,3 @@
+Title: Introductory Episode
+Date: 2015-03-30
+Category: Episodes
---
+++ pelicanconf.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Tobias Macey and Chris Patti'
+SITENAME = 'Podcast.__init__'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Renaissance Dev', 'http://blog.renaissancedev.com'),
+         ('Blind, Not Dumb', 'http://www.feoh.org'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/Podcast__init__'),)
+
+DEFAULT_PAGINATION = 10
+DEFAULT_CATEGORY = 'Episodes'
+
+THEME = 'pelican-themes/blueidea'
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+TWITTER_USERNAME = 'Podcast__init__'
