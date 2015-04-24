ad2d3e48f668dc179ae20ec59db0790c5873174a
Tobias Macey <tmacey@boundlessnotions.com>
2015-04-24 05:42:48 -0400

    Started using new theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,8 +31,15 @@ SOCIAL = (('Twitter', 'https://twitter.com/Podcast__init__'),)
 DEFAULT_PAGINATION = 10
 DEFAULT_CATEGORY = 'Episodes'
 
-THEME = 'pelican-themes/blueidea'
+THEME = 'pelican-themes/plumage'
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
 TWITTER_USERNAME = 'Podcast__init__'
+
+SITE_THUMBNAIL = '/images/podcast_init_logo.png'
+SITESUBTITLE = 'A podcast about Python and the people who make it great'
+RIGHT_SIDEBAR = """
+<a href="https://itunes.apple.com/us/podcast/podcast.-init/id981834425?mt=2&uo=6&at=&ct=" target="itunes_store" style="display:inline-block;overflow:hidden;background:url(http://linkmaker.itunes.apple.com/images/badges/en-us/badge_itunes-lrg.png) no-repeat;width:165px;height:40px;@media only screen{background-image:url(http://linkmaker.itunes.apple.com/images/badges/en-us/badge_itunes-lrg.svg);}"></a>
+<a href="http://www.stitcher.com/s?fid=64838&refid=stpr"><img src="http://cloudfront.assets.stitcher.com/promo.assets/stitcher-banner-120x90.jpg" width="120" height="90" alt="Listen to Stitcher"></a>
+"""
